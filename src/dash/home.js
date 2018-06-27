a62ed8b22f06573935fbb54e9d1db29eb3fb30f8
David Paul <dfp333@gmail.com>
2018-06-27 10:21:22 -0500

    added totals of projects and tools to home view, working on adding totals of unused tools

--- src/dash/home.js
+++ src/dash/home.js
@@ -8,46 +8,73 @@ class Home extends Component {
     state = {
         currentView: "Home",
         ownedBoxes:[],
+        ownedTotal:0,
+        unownedTotal: 0,
         unownedBoxes:[],
         projects:[],
         projectId:"",
-        refetch:"0"
+        projectTotal:0
 
     }
     componentDidMount() {
         fetch(`http://localhost:8088/tool?owner=${this.props.activeUser}&toolStatus=yes`)
-            .then(p=> p.json())
-            .then(tools => {
-                let box = []
-                    tools.forEach( tool => box.push(tool))
-                    this.setState({
-                        ownedBoxes: box
-                    })
+        .then(p=> p.json())
+        .then(tools => {
+            let box = []
+            let tot = 0
+            tools.forEach( tool => {
+                box.push(tool)
+                tot += parseInt(tool.toolPrice, 10)
             })
+            this.setState({
+                ownedBoxes: box,
+                ownedTotal: tot
+            })
+        })
         fetch(`http://localhost:8088/tool?owner=${this.props.activeUser}&toolStatus=no`)
         .then(p=> p.json())
         .then(tools => {
             let unbox = []
-                tools.forEach( tool => unbox.push(tool))
-                this.setState({
-                    unownedBoxes: unbox
-                })
+            let utot = 0
+            tools.forEach( tool => {
+                unbox.push(tool)
+                utot += parseInt(tool.toolPrice, 10)
+            })
+            this.setState({
+                unownedBoxes: unbox,
+                unownedTotal: utot
+            })
         })
 
         fetch(`http://localhost:8088/project?owner=${this.props.activeUser}`)
         .then(p=> p.json())
         .then(projects => {
             let proj = []
-                projects.forEach( project => proj.push(project))
-                this.setState({
-                    projects: proj
-                })
+            let ptot = 0
+            projects.forEach( project => {
+                proj.push(project)
+                ptot += parseInt(project.supplyCost, 10)
+            })
+            this.setState({
+                projects: proj,
+                projectTotal: ptot
+            })
         })
     }
 
 
     deleteTool = function (t){
         t.preventDefault()
+        this.setState({
+            ownedBoxes:[],
+            ownedTotal:0,
+            unownedTotal: 0,
+            unownedBoxes:[],
+            projects:[],
+            projectId:"",
+            projectTotal:0
+        })
+
         if (t.hasOwnProperty("target")){
             let id = t.target.id
             fetch(`http://localhost:8088/tool/${id}`,{
@@ -67,20 +94,46 @@ class Home extends Component {
                     .then(p=> p.json())
                     .then(tools => {
                         let box = []
-                            tools.forEach( tool => box.push(tool))
-                            this.setState({
-                                ownedBoxes: box
+                        let tot = 0
+                            tools.forEach( tool => {
+                                box.push(tool)
+                                tot += parseInt(tool.toolPrice, 10)
                             })
-                    })
-                .then(fetch(`http://localhost:8088/tool?owner=${this.props.activeUser}&toolStatus=no`)
-                .then(p=> p.json())
-                .then(tools => {
-                    let unbox = []
-                        tools.forEach( tool => unbox.push(tool))
-                        this.setState({
-                            unownedBoxes: unbox
+                            this.setState({
+                                ownedBoxes: box,
+                                ownedTotal: tot
+                            })
                         })
+                        .then(fetch(`http://localhost:8088/tool?owner=${this.props.activeUser}&toolStatus=no`)
+                        .then(p=> p.json())
+                        .then(tools => {
+                            let unbox = []
+                            let utot = 0
+                            tools.forEach( tool => {
+                                unbox.push(tool)
+                                utot += parseInt(tool.toolPrice, 10)
+                            })
+                            this.setState({
+                                unownedBoxes: unbox,
+                                unownedTotal: utot
+                            })
+                        }))
+
+                        .then(fetch(`http://localhost:8088/project?owner=${this.props.activeUser}`)
+                        .then(p=> p.json())
+                        .then(projects => {
+                            let proj = []
+                            let ptot = 0
+                            projects.forEach( project => {
+                                proj.push(project)
+                                ptot += parseInt(project.supplyCost, 10)
+                            })
+                            this.setState({
+                                projects: proj,
+                                projectTotal: ptot
+                            })
                 }))
+
             )
 
     }
@@ -98,6 +151,7 @@ class Home extends Component {
                                     <Delete id={t.id} onClick={this.deleteTool}></Delete>
                                 </Box>
                             ))}
+                            <Box key='owned_total' hasTextColor="info">Total: ${this.state.ownedTotal}</Box>
                         </Notification>
                     </Column>
                     <Column isSize='1/3'>Tools To Get:
@@ -108,13 +162,14 @@ class Home extends Component {
                                     <Delete id={t.id} onClick={this.deleteTool}></Delete>
                                 </Box>
                                 ))}
+                                <Box key='unowned_total' hasTextColor="info">Total: ${this.state.unownedTotal}</Box>
                         </Notification>
                     </Column>
                     <Column isSize='1/3'>Projects:
                         <Notification color="success">
                             {this.state.projects.map(p => (
 
-                                    <Box key={p.id}>
+                                <Box key={p.id}>
                                         {p.name}  <Button isSize='small'
                                                 isColor='success'
                                                 isOutlined
@@ -130,8 +185,10 @@ class Home extends Component {
                                                 >
                                             Details
                                         </Button>
+                                        <p>Supply Cost: ${p.supplyCost}</p>
                                     </Box>
                                 ))}
+                                <Box key='project_total' hasTextColor="info">Total: ${this.state.projectTotal}</Box>
                         </Notification>
                     </Column>
                 </Columns>
