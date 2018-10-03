a1cd04a01849d408194b457702a18e621786b2ea
David Paul <dfp333@gmail.com>
2018-10-03 05:00:06 -0500

    text editing on views.js

--- src/components/views.js
+++ src/components/views.js
@@ -2,8 +2,10 @@ import { Route } from 'react-router-dom'
 import React, { Component } from 'react'
 
 class Views extends Component {
-  View = () => {
-          return <Home activeUser={this.state.activeUser}
+  render() {
+
+    return(
+      <Home activeUser={this.state.activeUser}
             showView={this.showView}
             fetchHome={this.fetchHome}
             ownedBoxes={this.state.ownedBoxes}
@@ -13,19 +15,25 @@ class Views extends Component {
             projects={this.state.projects}
             projectId={this.state.projectId}
             projectTotal={this.state.projectTotal}
-            projectCost={this.projectCost}/>;
-          return <Tool activeUser={this.state.activeUser}
-            showView={this.showView}/>
-          return <Task activeUser={this.state.activeUser}
-            showView={this.showView}/>
-          return <Project {...this.state.viewProps}
-            activeUser={this.state.activeUser}
-            showView={this.showView}
-            handleChange={this.handleChange}
-            addTool={this.addTool}/>
-          return <strong>Please Log in!</strong>
-          return <Edit {...this.state.viewProps}
-            setActiveUser={this.setActiveUser}
-            showView={this.showView} />
+            projectCost={this.projectCost}/>
+
+      <Tool activeUser={this.state.activeUser}
+        showView={this.showView}/>
+
+      <Task activeUser={this.state.activeUser}
+        showView={this.showView}/>
+
+      <Project {...this.state.viewProps}
+        activeUser={this.state.activeUser}
+        showView={this.showView}
+        handleChange={this.handleChange}
+        addTool={this.addTool}/>
+
+      <strong>Please Log in!</strong>
+
+      <Edit {...this.state.viewProps}
+        setActiveUser={this.setActiveUser}
+        showView={this.showView} />
+    )
   }
 }
